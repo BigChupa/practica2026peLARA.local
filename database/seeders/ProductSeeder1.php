1e85037167b520addcf0f0db3790f03304af3ece
BigChupa <mikitapereverzev@gmail.com>
2026-04-27 20:06:26 +0300

    add productseeder

---
+++ database/seeders/ProductSeeder1.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder1 extends Seeder
+{
+
+public function run(): void
+{
+    $path = database_path('data/products.json');
+    $json = file_get_contents($path);
+    $products = json_decode($json, true);
+
+    foreach ($products as $item) {
+        // Используем updateOrCreate, чтобы не было дублей при повторном запуске
+        \App\Models\Product::updateOrCreate(
+            ['id' => $item['id']], // По какому полю искать
+            [
+                'name' => $item['name'],
+                'description' => $item['description'],
+                'price' => $item['price'],
+                'stock_quantity' => $item['stock_quantity'],
+                'sku' => $item['sku'],
+                'category_id' => $item['category_id'],
+                'image_path' => $item['image_path'],
+                'created_at' => now(), // или преобразуй дату из JSON
+                'updated_at' => now(),
+            ]
+        );
+    }
+}}
